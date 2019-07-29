20765a2b4f299962821baa94142cf2120217b5e6
zhengyix <zhengyix@uci.edu>
2019-07-29 22:53:02 +0800

    尝试weui组件

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -1,12 +1,32 @@
 <template>
     <div id="UserPage">
-        test
+        
     </div>
 </template>
 
 <script>
 export default {
-
+    name:"userPage",
+    mounted()
+    {
+        this.$weui.dialog({
+            title: '提示',
+            content: '是否领取礼品',
+            buttons: [{
+            label: '取消',
+            type: 'default',
+            onClick: () => {
+                alert('您已取消领取礼品！')
+            }
+            }, {
+                label: '确定',
+                type: 'primary',
+                onClick: () => {
+                    alert('您已确定领取礼品！')
+                }
+            }]
+        })
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import 'weui'
+import weui from 'weui.js'
 
 Vue.config.productionTip = false
+Vue.prototype.$weui = weui
 
 new Vue({
   router,
