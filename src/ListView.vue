6224a9a8cb24f8fe2b82fa79e9308d4d7a9e7763
Zhengyi Xu <zhengyix@uci.edu>
2019-07-29 14:43:59 +0800

    安装router和elementui

---
+++ src/ListView.vue
@@ -0,0 +1,22 @@
+<template>
+<div id="listview">
+</div>
+</template>
+
+<script>
+export default {
+    name:"listview",
+    data(){
+        return {
+            
+        }
+    },
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
